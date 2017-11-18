5207a45fb3386868c936149f712590d86d11ef1d
zhou-dong <82224165@qq.com>
2017-11-18 06:13:44 -0800

    add automake template

---
+++ src/algorithms/minimum-path-sum/redux/container.js
@@ -0,0 +1,76 @@
+import { connect } from "react-redux";
+
+import Table from "presentational/Table";
+import Buttons from "presentational/Buttons";
+import Dashboard from "presentational/Dashboard";
+import Modal from "presentational/Modal";
+import Header from "presentational/Header";
+
+import { buttonClick, closeModal, openModal, refreshData } from "./actions";
+
+export const createTable = () => {
+  const mapStateToProps = (state, ownProps) => ({
+    ...state,
+    table: state.minimumPathSumReducer.table,
+    styles: state.minimumPathSumReducer.styles
+  });
+  return connect(mapStateToProps, {})(Table);
+};
+
+export const createDashboard = () => {
+  const mapStateToProps = (state, ownProps) => ({
+    ...state,
+    score: state.minimumPathSumReducer.score,
+    errors: state.minimumPathSumReducer.errors,
+    steps: state.minimumPathSumReducer.steps
+  });
+  const mapDispatchToProps = dispatch => ({
+    onClick: () => {
+      dispatch(refreshData());
+    }
+  });
+  return connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+};
+
+export const createButtons = () => {
+  const mapStateToProps = state => ({
+    ...state,
+    buttons: state.minimumPathSumReducer.buttons
+  });
+  const mapDispatchToProps = dispatch => ({
+    onClick: buttonValue => {
+      dispatch(buttonClick(buttonValue));
+    }
+  });
+  return connect(mapStateToProps, mapDispatchToProps)(Buttons);
+};
+
+export const createModal = () => {
+  const mapStateToProps = state => ({
+    ...state,
+    showModal: state.minimumPathSumReducer.showModal,
+    closeModal: state.minimumPathSumReducer.closeModal,
+    modalTitle: state.minimumPathSumReducer.modalTitle,
+    modalBody: state.minimumPathSumReducer.modalBody
+  });
+  const mapDispatchToProps = dispatch => ({
+    closeModal: () => {
+      dispatch(closeModal());
+    }
+  });
+  return connect(mapStateToProps, mapDispatchToProps)(Modal);
+};
+
+export const createHeader = () => {
+  const mapStateToProps = state => ({
+    ...state,
+    title: state.minimumPathSumReducer.title,
+    openModal: state.minimumPathSumReducer.openModal
+  });
+  const mapDispatchToProps = dispatch => ({
+    openModal: () => {
+      dispatch(openModal());
+    }
+  });
+  return connect(mapStateToProps, mapDispatchToProps)(Header);
+};
--- src/tools/create-dp-template.js
+++ src/tools/create-dp-template.js
@@ -1,65 +1,134 @@
 const fs = require("fs");
 const path = require("path");
+const ejs = require("ejs");
 const chalk = require("chalk");
+const rimraf = require("rimraf");
 
-const green = chalk.green;
-const magenta = chalk.magenta;
-const yellow = chalk.yellow;
-const red = chalk.bold.red;
-const blue = chalk.blueBright;
-const log = console.log;
+const { mkdir, touch, cp, write } = require("./helper");
 
-// const name = process.argv[2];
-// if (!name) {
-//   red("Please type in algorithm name");
-//   process.exit();
-// }
+const inputName = process.argv[2];
+if (!inputName) {
+  throw "Please type in algorithm name";
+}
 
-const name = "dp-template";
-const createDir = (basePath, dirName) => {
-  log(magenta("making directory"), yellow(dirName), "at path", basePath);
-  const dirPath = path.join(basePath, dirName);
-  if (fs.existsSync(dirPath)) {
-    log(
-      red("errors......"),
-      yellow(dirName),
-      red("already exist in"),
-      basePath
-    );
-    process.exit();
-  }
-  fs.mkdirSync(dirPath);
-  log(green("created directory"), yellow(dirName), "at path", basePath);
-  return dirPath;
+const fileLoader = (basePath, fileName) => {
+  const filePath = path.join(basePath, fileName);
+  return ejs.fileLoader(filePath).toString("utf-8");
 };
 
-const createFile = (basePath, fileName) => {
-  log(blue("creating file"), yellow(fileName), "at path", basePath);
-  const filepath = path.join(basePath, fileName);
-  fs.closeSync(fs.openSync(filepath, "w"));
-  log(green("created file"), yellow(fileName), "at path", basePath);
-  return filepath;
+const findHyphens = array => {
+  const indexes = [];
+  array.forEach((ch, i) => {
+    if (ch === "-") {
+      indexes.push(i);
+    }
+  });
+  return indexes;
 };
 
-const workingDirectory = path.join(path.resolve(__dirname));
-log(blue("WorkingDirectory"), yellow(workingDirectory));
+const camelName = () => {
+  const array = inputName.split("");
+  findHyphens(array).forEach(i => {
+    array[i + 1] = array[i + 1].toUpperCase();
+  });
+  return array.filter(ch => ch !== "-").join("");
+};
 
-const baseDir = createDir(workingDirectory, name);
-createFile(baseDir, "algorithm.js");
-createFile(baseDir, "constants.js");
-createFile(baseDir, "helper.js");
-createFile(baseDir, "index.js");
+const createTitle = () => {
+  const array = inputName.split("");
+  array[0] = array[0].toUpperCase();
+  findHyphens(array).forEach(i => {
+    array[i + 1] = array[i + 1].toUpperCase();
+  });
+  return array.map(ch => (ch === "-" ? " " : ch)).join("");
+};
 
-const reduxDir = createDir(baseDir, "redux");
-createFile(reduxDir, "actions.js");
-createFile(reduxDir, "container.js");
-createFile(reduxDir, "reducer.js");
-createFile(reduxDir, "update-steps.js");
+const UNDERSCORE_UPPERCASE_NAME = () =>
+  inputName
+    .split("")
+    .map(ch => (ch === "-" ? "_" : ch.toUpperCase()))
+    .join("");
 
-const mockDir = createDir(baseDir, "__mock__");
-createFile(mockDir, `${name}-mock.json`);
+const createReducerName = () => `${camelName()}Reducer`;
 
-const testsDir = createDir(baseDir, "__tests__");
-createFile(testsDir, `${name}.spec.js`);
+const srcPath = path.join(path.resolve(__dirname), "../");
 
-log(green("Create Template Finished :)"));
+const templateDir = path.join(srcPath, "tools", "dp-template");
+const templateReduxDir = path.join(templateDir, "redux");
+
+if (process.argv[3] === "force") {
+  const toBeRemoved = path.join(srcPath, "algorithms", inputName);
+  rimraf.sync(toBeRemoved, fs);
+  console.log(chalk.bold.red(`removed directory: ${toBeRemoved}`));
+}
+
+const destDir = mkdir(path.join(srcPath, "algorithms"), inputName);
+const reduxDir = mkdir(destDir, "redux");
+const mockDir = mkdir(destDir, "__mock__");
+const testsDir = mkdir(destDir, "__tests__");
+
+const mockFileName = `${inputName}-mock.json`;
+write(path.join(mockDir, mockFileName), "{}");
+touch(testsDir, `${inputName}.spec.js`);
+
+const renderHelperJs = () => {
+  const ejsTemplate = fileLoader(templateDir, "helper.ejs");
+  const render = ejs.render(ejsTemplate, {
+    mockFileName: mockFileName,
+    title: createTitle()
+  });
+  write(path.join(destDir, "helper.js"), render);
+};
+
+const cpIndexJs = () => {
+  const source = path.join(templateDir, "index.js");
+  const target = path.join(destDir, "index.js");
+  cp(source, target);
+};
+
+const renderActionsJs = () => {
+  const ejsTemplate = fileLoader(templateReduxDir, "actions.ejs");
+  const render = ejs.render(ejsTemplate, {
+    UNDERSCORE_UPPERCASE_NAME: UNDERSCORE_UPPERCASE_NAME()
+  });
+  write(path.join(reduxDir, "action.js"), render);
+};
+
+const renderContainerJs = () => {
+  const ejsTemplate = fileLoader(templateReduxDir, "container.ejs");
+  const render = ejs.render(ejsTemplate, { reducerName: createReducerName() });
+  write(path.join(reduxDir, "container.js"), render);
+};
+
+const cpReducerJs = () => {
+  const source = path.join(templateReduxDir, "reducer.js");
+  const target = path.join(reduxDir, "reducer.js");
+  cp(source, target);
+};
+
+const cpUpdateStepsJs = () => {
+  const source = path.join(templateReduxDir, "update-steps.js");
+  const target = path.join(reduxDir, "update-steps.js");
+  cp(source, target);
+};
+
+const cpAlgorithmJs = () => {
+  const source = path.join(templateDir, "algorithm.js");
+  const target = path.join(destDir, "algorithm.js");
+  cp(source, target);
+};
+
+const cpConstantsJs = () => {
+  const source = path.join(templateDir, "constants.js");
+  const target = path.join(destDir, "constants.js");
+  cp(source, target);
+};
+
+cpIndexJs();
+renderHelperJs();
+renderActionsJs();
+renderContainerJs();
+cpReducerJs();
+cpUpdateStepsJs();
+cpAlgorithmJs();
+cpConstantsJs();

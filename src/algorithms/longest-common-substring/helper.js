d8ab7e81d59d0aa8711fd98a113e681f0ed74026
zhou-dong <82224165@qq.com>
2017-11-20 10:23:21 -0800

    init longest common substring

--- src/algorithms/longest-common-substring/helper.js
+++ src/algorithms/longest-common-substring/helper.js
@@ -1,36 +1,58 @@
 import {
-  createDPTableWithIndicator,
+  createDPTableWithoutIndicator,
   createStyleTableWithIndicator
 } from "utils/dp-helper";
 
 import mock from "./__mock__/longest-common-substring-mock.json";
 import createComparedTable from "./algorithm";
 import { modalBody } from "./introduction";
+import { longestString } from "utils/generic-helper";
+import {
+  TABLE_ELEMENT_SUCCESS_STYLE,
+  TABLE_ELEMENT_DISABLE_STYLE
+} from "presentational/constants";
 
-const createButtons = () => {};
+const createButtons = (str1, str2) => {
+  const length = longestString(str1, str2).length + 1;
+  return Array.from(Array(length).keys());
+};
 
-const createDisplayTable = () => {};
+const createDisplayTable = (str1, str2) => {
+  const colLen = str2.length + 2;
+  const table = createDPTableWithoutIndicator(str2, str1);
+  table[1] = Array(colLen).fill(0);
+  table.forEach(row => (row[1] = 0));
+  table[1][0] = "";
+  table[0][1] = "";
+  return table;
+};
 
-const createStyleTable = () => {};
+const createStyleTable = (str1, str2) => {
+  const colLen = str2.length + 2;
+  const table = createStyleTableWithIndicator(str2, str1);
+  table[1] = Array(colLen).fill(TABLE_ELEMENT_SUCCESS_STYLE);
+  table.forEach(row => (row[1] = TABLE_ELEMENT_SUCCESS_STYLE));
+  table[0][1] = TABLE_ELEMENT_DISABLE_STYLE;
+  table[1][0] = TABLE_ELEMENT_DISABLE_STYLE;
+  return table;
+};
 
-const getTotalScore = () => {};
-
-const getStartRowIndex = () => {};
-
-const getStartColIndex = () => {};
+const getTotalScore = (str1, str2) => longestString(str1, str2).length;
 
 export default () => {
+  const str1 = mock.s1;
+  const str2 = mock.s2;
   return {
-    table: createDisplayTable(),
-    styles: createStyleTable(),
+    table: createDisplayTable(str1, str2),
+    styles: createStyleTable(str1, str2),
     compared: createComparedTable(),
-    score: getTotalScore(),
-    buttons: createButtons(),
+    score: getTotalScore(str1, str2),
+    buttons: createButtons(str1, str2),
     title: "Longest Common Substring",
     modalTitle: "Longest Common Substring",
     modalBody: modalBody,
-    row: getStartRowIndex(),
-    col: getStartColIndex(),
+    row: 2,
+    col: 2,
     steps: 0,
     errors: 0,
     showModal: false

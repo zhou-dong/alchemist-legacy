dcf30dc5295a8b4dcaf82837b76598e41645889c
zhou-dong <82224165@qq.com>
2017-12-01 22:51:51 -0800

    correct maximum subarray

--- src/algorithms/maximum-subarray-problem/algorithm.js
+++ src/algorithms/maximum-subarray-problem/algorithm.js
@@ -5,21 +5,21 @@ export default array => {
   const starts = ["START", 1];
   const ends = ["END", 1];
 
+  let lastStart = 1;
   array.forEach((value, index) => {
     if (index < 2) return;
     let [currentMax, globalMax, end] = [0, 0, 0];
     let start = starts[index - 1];
     if (value > value + currentMaxs[index - 1]) {
       currentMax = value;
-      if (value > globalMaxs[index - 1]) {
-        start = index;
-      }
+      lastStart = index;
     } else {
       currentMax = value + currentMaxs[index - 1];
     }
     if (currentMax > globalMaxs[index - 1]) {
       globalMax = currentMax;
       end = index;
+      start = lastStart;
     } else {
       globalMax = globalMaxs[index - 1];
       end = ends[index - 1];

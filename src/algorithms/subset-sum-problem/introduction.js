e65dfd1dbaa23bdaae069c0a5a156b885a2a4417
zhou-dong <82224165@qq.com>
2017-12-02 02:57:45 -0800

    add intro to Subset Sum Problem

--- src/algorithms/subset-sum-problem/introduction.js
+++ src/algorithms/subset-sum-problem/introduction.js
@@ -1,16 +1,26 @@
 const formula = `
-<pre><code>
-  ... code ...
+<pre><code>if (element > weight) {
+    table[row][col] = table[row - 1][col]
+} else {
+    table[row][col] = table[row - 1][col] || table[row - 1][weight - element]
+}
 </code></pre>
 `;
 
-const introduction = "... introduction ...";
+const introduction =
+  "Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.";
+
+const example = `<b>Examples</b>:
+<pre><code>set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output: True //subset(4, 5) with sum 9
+</code></pre>
+`;
 
 export const modalBody = `
 <b>Introduction: </b>
 </br>
 ${introduction}
 </br>
+${example}
 <b>Formula: </b>
-</br>
 ${formula}`;

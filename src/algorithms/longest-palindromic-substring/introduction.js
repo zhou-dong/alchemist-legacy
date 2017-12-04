c9394adca265a6037d916b36be328580b3bdda98
zhou-dong <82224165@qq.com>
2017-12-03 17:04:33 -0800

    add introduction to longest palindrome substring

--- src/algorithms/longest-palindromic-substring/introduction.js
+++ src/algorithms/longest-palindromic-substring/introduction.js
@@ -1,16 +1,29 @@
 const formula = `
-<pre><code>
-  ... code ...
+<pre><code>if (front === end) {
+    table[i][i + len - 1] =
+        table[i + 1][i + len - 2];
+} else {
+    table[i][i + len - 1] = false;
+}
 </code></pre>
 `;
 
-const introduction = "... introduction ...";
+const introduction =
+  "Given a string, find the length of longest palindromic substring.";
 
+const example = `<b>Example:</b>
+<pre><code>Example:
+Input: "dbcdcd"
+Output: 3
+Note: "cdc", "dcd" with length 3
+</code></pre>
+`;
 export const modalBody = `
 <b>Introduction: </b>
 </br>
 ${introduction}
 </br>
+${example}
 <b>Formula: </b>
 </br>
 ${formula}`;

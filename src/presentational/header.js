21ff6a8487c883eaeb6fc47591a48d24f84fbd1c
zhou-dong <82224165@qq.com>
2018-04-19 14:17:48 -0700

    test sucess icon

--- src/presentational/header.js
+++ src/presentational/header.js
@@ -1,24 +1,39 @@
 // @flow
 
 import React from "react";
-import { Glyphicon } from "react-bootstrap";
+import { Glyphicon, Label } from "react-bootstrap";
 
 export type Props = {
   title: string,
-  openModal: Function
+  openModal: Function,
+  count: number
+};
+
+const getSuccessCount = (props: Props) => {
+  const count = props && props.count;
+  if (count) {
+    return (
+      <small>
+        <Label bsStyle="success">
+          <Glyphicon glyph="ok" />
+          {count}
+        </Label>
+      </small>
+    );
+  }
 };
 
 export default (props: Props) => (
   <div>
     <header>
       <h1>
-        <small>
-          <Glyphicon glyph="glyphicon-ok" />
-        </small>
-        {props.title}{" "}
+        <span>{props.title}&nbsp;</span>
         <small onClick={props.openModal}>
-          <Glyphicon glyph="question-sign" />
+          <Label bsStyle="info">
+            <Glyphicon glyph="question-sign" />
+          </Label>&nbsp;
         </small>
+        {getSuccessCount(props)}
       </h1>
     </header>
     <hr />
